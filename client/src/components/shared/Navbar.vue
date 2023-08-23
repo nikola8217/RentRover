c9fe85c117c78d891644a29d36215ec3ea073354
nikola8217 <gileer404@gmail.com>
2023-08-23 12:16:15 +0200

    Header

---
+++ client/src/components/shared/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <header class="transparent header-light scroll-light has-topbar">
+        <div id="topbar" class="topbar-dark text-light">
+            <div class="container">
+                <div class="topbar-left xs-hide">
+                    <div class="topbar-widget">
+                        <div class="topbar-widget"><a href="#"><i class="fa fa-phone"></i>+381 62 8 254 103</a></div>
+                        <div class="topbar-widget"><a href="#"><i class="fa fa-envelope"></i>gileer404@gmail.com</a></div>
+                        <div class="topbar-widget"><a href="#"><i class="fa fa-clock-o"></i>Mon - Fri 09.00 - 17.00</a></div>
+                    </div>
+                </div>
+            
+                <div class="topbar-right">
+                    <div class="social-icons">
+                        <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+                        <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                        <a href="#"><i class="fa fa-youtube fa-lg"></i></a>
+                        <a href="#"><i class="fa fa-pinterest fa-lg"></i></a>
+                        <a href="#"><i class="fa fa-instagram fa-lg"></i></a>
+                    </div>
+                </div>  
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="de-flex sm-pt10">
+                        <div class="de-flex-col">
+                            <div class="de-flex-col">
+                               
+                                <div id="logo">
+                                    <h3 class="mt-3">RentRover</h3>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div class="de-flex-col header-col-mid">
+                            <ul id="mainmenu">
+                                <li><a class="menu-item" href="booking.html">Home</a></li>
+                                <li><a class="menu-item" href="booking.html">About</a></li>
+                                <li><a class="menu-item" href="booking.html">Cars</a></li>
+                                <li><a class="menu-item" href="booking.html">Contact</a></li>
+                                <li><a class="menu-item" href="booking.html">Blog</a></li>
+                                <li><a class="menu-item" href="booking.html">Login</a></li>
+                                <li><a class="menu-item" href="booking.html">Register</a></li>
+                            </ul>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
